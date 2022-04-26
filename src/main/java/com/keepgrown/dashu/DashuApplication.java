89fb3c612eb1bf1241af0acc24cc5eecf5076699
ziro <877185959@qq.com>
2022-04-26 22:32:59 +0800

    Hello World.

---
+++ src/main/java/com/keepgrown/dashu/DashuApplication.java
@@ -0,0 +1,13 @@
+package com.keepgrown.dashu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashuApplication.class, args);
+    }
+
+}
