796b55e32f8dfeaeffc3484e2e6595d88488fc30
ziro <877185959@qq.com>
2022-04-29 23:23:16 +0800

    Update: 配置扫描包

--- module/src/main/java/com/kg/dashu/DashuApplication.java
+++ module/src/main/java/com/kg/dashu/DashuApplication.java
@@ -2,14 +2,12 @@ package com.kg.dashu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kg.**")
 public class DashuApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(DashuApplication.class, args);
-        System.out.println(111);
+        SpringApplication.run(DashuApplication.class, args);
     }
 
 }
