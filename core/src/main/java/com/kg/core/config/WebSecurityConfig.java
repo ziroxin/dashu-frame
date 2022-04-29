1845f8891889cae73e3143d4dc28e58eff75f5c9
ziro <877185959@qq.com>
2022-04-29 23:23:47 +0800

    Add: 配置Security的config

--- core/src/main/java/com/kg/core/config/WebSecurityConfig.java
+++ core/src/main/java/com/kg/core/config/WebSecurityConfig.java
@@ -1,13 +1,26 @@
 package com.kg.core.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  * Serurity 配置
  *
  * @author ziro
  * @date 2022/4/27 23:08
  */
-//@Configuration
-public class WebSecurityConfig {//extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 配置默认解密方法
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
