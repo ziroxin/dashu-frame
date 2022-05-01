3f3a5afaf63cda87a2dedb737eec6a68cea3e9df
ziro <877185959@qq.com>
2022-05-01 21:58:05 +0800

    Add: 增加swagger配置；增加mybatis plus代码生成器

---
+++ module/src/test/java/com/kg/dashu/MybatisPlusGenerator.java
@@ -0,0 +1,56 @@
+package com.kg.dashu;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
+
+/**
+ * 基于Mybatis plus的代码生成器
+ *
+ * @author ziro
+ * @date 2022/4/30 14:29
+ * @see <a href="https://www.mybatis-plus.com/guide/generator-new.html#%E5%BF%AB%E9%80%9F%E5%85%A5%E9%97%A8">代码生成器入门</a>
+ */
+@SpringBootTest
+public class MybatisPlusGenerator {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String dbUserName;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Test
+    public void generator() {
+        // 指定输出目录
+        String basePath = "C:\\Users\\Administrator\\IdeaProjects\\dashu\\core\\src\\main\\java";
+        // 配置生成器
+        FastAutoGenerator.create(dbUrl, dbUserName, dbPassword)
+                .globalConfig(builder -> {
+                    builder.author("ziro") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+
+
+
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(basePath); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.kg.core") // 设置父包名
+                            .moduleName("zuser"); // 设置父包模块名
+//                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, basePath + "\\com\\kg\\core\\zuser\\mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("z_user"); // 设置需要生成的表名
+//                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
