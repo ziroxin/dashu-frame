91dc5b235cb2fd03c9cc38e974c318b14e8c8060
ziro <877185959@qq.com>
2022-05-06 20:38:03 +0800

    Update: 优化security异常处理

--- core/src/main/java/com/kg/core/security/handler/SimpleAccessDeniedHandler.java
+++ core/src/main/java/com/kg/core/security/handler/SimpleAccessDeniedHandler.java
@@ -1,33 +1,31 @@
 package com.kg.core.security.handler;
 
+import com.kg.component.utils.ResponseWriteUtils;
 import com.kg.core.web.ResponseResult;
-import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * 访问被拒绝拦截器
+ *
  * @author ziro
  * @date 2022/5/5 22:50
  */
 @Component
 public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
-    @SneakyThrows
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.setStatus(HttpStatus.OK.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().print(ResponseResult.builder()
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        String result = ResponseResult.builder()
                 .code("" + HttpStatus.FORBIDDEN.value())
                 .message("访问被拒绝！您的权限不足")
-                .toJsonString());
+                .toJsonString();
+        ResponseWriteUtils.writeJson200(response, result);
     }
 }
